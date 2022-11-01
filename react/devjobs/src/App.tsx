5b59d8e198222de633192e682dd0eb5a2ef96d6d
Ceasar Barbosa <area21software@gmail.com>
2022-11-01 11:52:11 -0500

    tailwind css setup

--- react/devjobs/src/App.tsx
+++ react/devjobs/src/App.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const App = () => {
-	return <div>Hola</div>;
+    return (
+        <div>
+            <h1 className="text-9xl font-bold text-slate-800 underline">
+                Hello world!
+            </h1>
+        </div>
+    );
 };
 
 export default App;
