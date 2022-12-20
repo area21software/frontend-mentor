cc35bcce7dcf57a8c1eb3edbd281ee9d8d5612cb
Ceasar Barbosa <area21software@gmail.com>
2022-12-20 11:20:29 -0600

    multi-step-form component

--- react/components/multi-step-form/src/App.tsx
+++ react/components/multi-step-form/src/App.tsx
@@ -1,5 +1,5 @@
 const App = () => {
-  return <div>Hello</div>
+  return <div className="text-blue-300 border-lime-100">App</div>
 }
 
 export default App
--- react/components/multi-step-form/src/index.tsx
+++ react/components/multi-step-form/src/index.tsx
@@ -2,8 +2,6 @@ import { createRoot } from "react-dom/client"
 import App from "./App"
 
 const container = document.getElementById("root")
-if (!container) throw Error("No root container found")
-
+if (!container) throw Error("Cant get app container element")
 const root = createRoot(container)
-
 root.render(<App />)
