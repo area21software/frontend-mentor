ba27bcea4ec84cf64adc67eb88f4a69a88de1a9c
Ceasar Barbosa <area21software@gmail.com>
2023-01-04 16:20:50 -0600

    add dropdown to categories

--- react/product-feedback/src/components/FeedbackForm.tsx
+++ react/product-feedback/src/components/FeedbackForm.tsx
@@ -3,17 +3,27 @@ import { IoIosArrowBack } from "react-icons/io"
 import { IoIosArrowDown } from "react-icons/io"
 import { IoIosArrowUp } from "react-icons/io"
 import IconNewFeedback from "../assets/shared/icon-new-feedback.svg"
+import IconCheck from "../assets/shared/icon-check.svg"
 
 import { useNavigate } from "react-router-dom"
 
+const categories = ["Feature", "UI", "UX", "Enhancement", "Bug"]
+
 const FeedbackForm = () => {
   const [isCategoryOpen, setIsCategoryOpen] = useState(false)
+  const [selectedCategory, setCategory] = useState("Feature")
   // WIll NEED FOR EDIT FEEDBACK
   //   const [isStatusOpen, setIsStatusOpen] = useState(false)
   const navigate = useNavigate()
 
-  const handleIsCategoryOpen = () => {
+  const toggleCategoryDropdown = () => {
     setIsCategoryOpen((prevValue) => !prevValue)
+  }
+
+  const handleSelected = (index: number) => {
+    const selectedCategory = categories[index]
+    setCategory(selectedCategory)
+    toggleCategoryDropdown()
   }
 
   // WIll NEED FOR EDIT FEEDBACK
@@ -22,7 +32,7 @@ const FeedbackForm = () => {
   //   }
 
   return (
-    <>
+    <div className="max-w-[540px] m-auto">
       <button
         className="flex items-center text-pfGrey font-bold text-b3 pt-9 ml-6"
         onClick={() => navigate(-1)}
@@ -49,7 +59,7 @@ const FeedbackForm = () => {
               </p>
               <textarea className="h-12 bg-pfOffWhite rounded-lg mt-4" />
             </div>
-            <div className="flex flex-col mt-6">
+            <div className="relative flex flex-col mt-6">
               <h2 className="font-bold text-pfDarkBlueGrey2 text-b3">
                 Category
               </h2>
@@ -57,17 +67,36 @@ const FeedbackForm = () => {
                 Choose a category for your feedback
               </p>
               <button
-                className="inline-flex pl-6 bg-pfOffWhite text-b2 text-pfDarkBlueGrey2 rounded-md h-12 items-center justify-between mt-4"
-                onClick={handleIsCategoryOpen}
+                className="relative inline-flex pl-6 bg-pfOffWhite text-b2 text-pfDarkBlueGrey2 rounded-md h-12 items-center justify-between mt-4"
+                onClick={toggleCategoryDropdown}
               >
-                Feature
+                {selectedCategory}
                 {isCategoryOpen ? (
                   <IoIosArrowUp className="fill-pfDarkBlue mr-6" />
                 ) : (
                   <IoIosArrowDown className="fill-pfDarkBlue mr-6" />
                 )}
               </button>
-              {isCategoryOpen && <div className="h-20 w-20">category</div>}
+              {isCategoryOpen && (
+                <div className="absolute -bottom-64 h-60  shadow-md bg-white rounded-lg w-full">
+                  <ul>
+                    {categories.map((category, index) => (
+                      <div className="flex items-center justify-between border-b-[1px] border-pfOffWhite w-full">
+                        <li
+                          key={index}
+                          className="text-b1 font-regular pl-6 py-3 w-full"
+                          onClick={() => handleSelected(index)}
+                        >
+                          {category}
+                        </li>
+                        {selectedCategory === categories[index] ? (
+                          <img src={IconCheck} className="h-2 w-3 mr-6" />
+                        ) : null}
+                      </div>
+                    ))}
+                  </ul>
+                </div>
+              )}
             </div>
             <div className="flex flex-col mt-6">
               <h2 className="font-bold text-pfDarkBlueGrey2 text-b3">
@@ -90,7 +119,7 @@ const FeedbackForm = () => {
           </div>
         </div>
       </div>
-    </>
+    </div>
   )
 }
 
